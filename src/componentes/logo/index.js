8c0ba68a11dd26df0db6839bed640ecd8c0991b6
Vitor <vitordiasalves@gmail.com>
2025-06-23 15:36:44 -0300

    update: adiciona cor para o App e para o Header

--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -5,7 +5,7 @@ import logo from '../../imagens/logo.svg';
 function Logo() {
     return (
         <div className='logo'>
-            <img src={logo} alt='logo'></img>
+            <img src={logo} alt='logo'className='logo-img'/>
             <p><strong>React</strong> Books</p>
         </div>
     );
