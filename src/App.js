965507c19551606f1542c878d815862057e94715
Vitor <vitordiasalves@gmail.com>
2025-06-23 14:38:04 -0300

    feat: adiciona o componente logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Logo from './componentes/logo/index';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Logo></Logo>
         
       </header>
     </div>
