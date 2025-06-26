3999c2bcc5650b1ee96f414d00f23984a235c295
Vitor Dias <vitordiasalves@gmail.com>
2025-06-26 10:33:15 -0300

    update: estilos substituidos por styled container em Logo e OpcoesHeader

--- src/componentes/IconesHeader/index.js
+++ src/componentes/IconesHeader/index.js
@@ -1,16 +1,25 @@
-import './estilo.css'
 import iconePerfil from '../../imagens/perfil.svg';
 import iconeSacola from '../../imagens/sacola.svg';
+import styled from 'styled-components';
+
+const Icone = styled.li`
+    margin: 0px 20px;
+    width: 25px;
+`
+const Icones = styled.ul`
+    display: flex;
+    align-items: center;
+`
 
 const icones = [iconePerfil, iconeSacola];
 
 function IconesHeader() {
     return (
-        <ul className='icones'>
+        <Icones>
             {icones.map((icone, index) => (
-                <li className='icone' key={index}><img src={icone} alt='icone'></img></li>
+                <Icone key={index}><img src={icone} alt='icone'></img></Icone>
             ))}
-        </ul>
+        </Icones>
     )
 } 
 
--- src/componentes/OpcoesHeader/index.js
+++ src/componentes/OpcoesHeader/index.js
@@ -1,14 +1,29 @@
-import './estilo.css'
+import styled from 'styled-components';
+
+const Opcoes = styled.ul`
+  display: flex;
+`
+const Opcao = styled.div`
+  min-width: 120px;
+  display: flex;
+  font-size: 16;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  padding: 0 5px;
+  cursor: pointer;
+`
 
 const textoOpcoes = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE'];
 
 function OpcoesHeader() {
     return (
-        <ul className='opcoes'>
+        <Opcoes>
             {textoOpcoes.map((texto, index) => (
-                <li className='opcao' key={index}><p>{texto}</p></li>
+                <Opcao key={index}><p>{texto}</p></Opcao>
             ))}
-        </ul>
+        </Opcoes>
     )
 }
 
