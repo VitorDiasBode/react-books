0db281aaea474810a93faf8428adc8223581c9ea
Vitor Dias <vitordiasalves@gmail.com>
2025-07-01 18:44:47 -0300

    update: implementa buscar livros com o componente Pesquisa
    - No componente pesquisa é adicionado uma lista de objeto com os dados dos livros
    - Quando o input do componente Pesquisa é modificado, um busca é feita para mostrar os livros com nome que foi digitado.

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,5 +1,7 @@
 import Input from '../Input';
 import styled from 'styled-components';
+import { useState } from 'react';
+import { livros } from './dadosPesquisa'
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -24,13 +26,62 @@ const Subtitulo = styled.h3`
     margin-bottom:40px;
 `
 
+const ListaLivros = styled.ul`
+    justify-content: center;
+    max-width: 1170px;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 32px;
+    margin-top: 32px;
+`
+
+const Livro = styled.li`
+    width: 210px;
+    background-color:rgba(17, 17, 17, 0.31);
+    border-radius: 20px;
+    box-shadow: 0 6px 12px rgb(0,0,0,0.2);
+    transition: 0.2s;
+    padding: 20px;
+
+    p{
+        letter-spacing: 2px;
+        font-weight: 700;
+    } 
+
+`
 
 function Pesquisa(){
+    const [livrosPesquisados, setLivrosPesquisados] = useState([]);
+    console.log(livrosPesquisados);
+    
     return (
         <PesquisaContainer>
             <Titulo>Qual livro você está buscando?</Titulo>
             <Subtitulo>Encontre-o aqui</Subtitulo>
-            <Input placeholder="Escreva o livro que você busca"/>
+            <Input
+                placeholder="Escreva o livro que você busca"
+                onChange={evento =>{
+                    const textoDigitado = evento.target.value;
+                    const resultadoPesquisa = livros.filter( livro => {
+                        return livro.nome
+                        .toLocaleLowerCase()
+                        .includes(textoDigitado.toLowerCase())
+                    });
+                    setLivrosPesquisados(resultadoPesquisa);
+                }}
+            />
+            <h1>Livros</h1>
+            <ListaLivros>
+                {livrosPesquisados.map( livro => (
+                    <Livro>
+                        <img src={livro.src} alt={'img'+livro.nome}></img>
+                        <p>{livro.nome}</p>
+                    </Livro>
+                ))}
+
+            </ListaLivros>
+            
 
         </PesquisaContainer>
     )
