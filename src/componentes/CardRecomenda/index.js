5d5039ad9203928ab774d22e6015d180171bd266
Vitor <vitordiasalves@gmail.com>
2025-07-22 14:00:24 -0300

    feat: adiciona a seção de livros recomendados

---
+++ src/componentes/CardRecomenda/index.js
@@ -0,0 +1,68 @@
+import styled from "styled-components"
+
+const Card = styled.div`
+    align-items: center;
+    background-color: #FFF;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    display: flex;
+    margin: 10px auto;
+    max-width: 600px;
+    padding: 25px 20px;
+    justify-content: space-around;
+    width: 100%;
+`
+
+const Botao = styled.button`
+    background-color: #EB9B00;
+    color: #FFF;
+    padding: 10px 0px;
+    font-size: 16px;
+    border: none;
+    font-weight: 900;
+    display: block;
+    text-align: center;
+    width: 150px;
+    border-radius: 5px;
+    &hover {
+        cursor: pointer;
+    }
+`
+const Descricao = styled.p`
+    width: 90%;
+    height: 100%;
+`
+const Subtitulo = styled.h4`
+    color: #002F52;
+    font-size: 18px;
+    font-weight: bold;
+    margin: 15px 0;
+`
+const ImgLivro = styled.img`
+    width: 150px;
+`
+const DivTextos = styled.div`
+    width: 100%;
+    height: 100%;
+`
+
+
+
+function CardRecomenda({titulo, subtitulo, descricao, img}){
+    return(
+        <Card>
+            <DivTextos>
+                <h3>{titulo}</h3>
+                <Subtitulo>{subtitulo}</Subtitulo>
+                <Descricao>{descricao}</Descricao>
+            </DivTextos>
+            <div>
+                <ImgLivro src={img} alt="capa do livro recomendado"></ImgLivro>
+                <Botao>Saiba mais</Botao>
+            </div>
+        </Card>
+    )
+}
+
+export default CardRecomenda
+
--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,6 +1,8 @@
 import { livros } from "./dadosUltimosLancamentos";
 import styled from "styled-components";
 import { Titulo } from "../Titulo";
+import CardRecomenda from "../CardRecomenda";
+import imgLivro from "../../imagens/capa-indisponivel.png" 
 
 const ContainerUltimosLancamentos = styled.section`
     background-color:#EBECEE;
@@ -27,6 +29,20 @@ function UltimosLancamentos() {
             <NovosLivrosContainer>
                 { livros.map(livro => <img src={livro.src} alt="capa indisponivel"/> ) }
             </NovosLivrosContainer>
+
+            <CardRecomenda 
+            titulo={"Talvez você se interesse por..."}
+            subtitulo={"Mistborn: História Secreta"}
+            descricao={"Do autor best-seller nº 1 do New York Times, Mistborn: História Secreta destaca personagens e fatos escondidos nas sombras da Trilogia Original Mistborn."}
+            img={imgLivro}
+            ></CardRecomenda>
+            
+            <CardRecomenda 
+            titulo={"Outro livro que talvez você se interesse por..."}
+            subtitulo={"Filha do Caos: 1"}
+            descricao={"Na Grécia Antiga, os mortais vivem sob o jugo cruel dos deuses, perdendo o que têm de mais valioso o alimento que os sustenta, seus bens mais preciosos e, às vezes, até a própria vida para satisfazer os caprichos dos Doze. Mas uma antiga profecia traz uma fagulha de esperança: a promessa de alguém destinado a libertar a humanidade."}
+            img={imgLivro}
+            ></CardRecomenda>
         </ContainerUltimosLancamentos>
     )
 }
