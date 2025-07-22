0f323573397709aec76515b31734e1520d14e5d1
Vitor <vitordiasalves@gmail.com>
2025-07-22 09:51:10 -0300

    feat: implementa o componente Titulo - Componente tem 3 propriedades que podem declarados com propriedades: - - cor - - tamanhoFonte - - align

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,20 +1,12 @@
 import { livros } from "./dadosUltimosLancamentos";
 import styled from "styled-components";
+import { Titulo } from "../Titulo";
 
 const ContainerUltimosLancamentos = styled.section`
     background-color:#EBECEE;
     padding-bottom: 20px;
     display: flex;
     flex-direction: column; 
-`
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0px;
-    background-color: #FFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 const NovosLivrosContainer = styled.div`
     margin-top: 30px;
@@ -28,7 +20,10 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return(
         <ContainerUltimosLancamentos>
-            <Titulo>ULTIMOS LANÇAMENTOS</Titulo>
+            <Titulo 
+                cor="#EB9B00"
+                tamanhoFonte="36px"
+            >ULTIMOS LANÇAMENTOS</Titulo>
             <NovosLivrosContainer>
                 { livros.map(livro => <img src={livro.src} alt="capa indisponivel"/> ) }
             </NovosLivrosContainer>
